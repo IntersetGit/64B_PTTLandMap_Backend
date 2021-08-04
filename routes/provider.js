554d23c5689c20867670c1e81bf941aebdc96a3a
C.chanaphat <yochi.suz@gmail.com>
2021-08-04 16:08:41 +0700

    .

--- routes/provider.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const providerControllers = require("../controllers/providerControllers");
-
-/* GET users listing. */
-router.post('/login', providerControllers.login);
-
-module.exports = router;
