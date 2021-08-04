d665a41900ff54160c2b659ee0dd4f32c365961e
mpat1234 <Psanorit@gmail.com>
2021-08-04 17:52:05 +0700

    mastername

--- controllers/masterControllers.js
+++ controllers/masterControllers.js
@@ -1,19 +1,14 @@
-const models = require("../models/index"); 
-const sequelize = require("../config/dbConfig"); 
+const models = require("../models/index");
+const sequelize = require("../config/dbConfig");
 const messages = require("../messages/index");
 const result = require("../middleware/result");
+const { GetAllTitleNameService } = require("../service/mas_name_titles");
 
 exports.testt = async (req, res, next) => {
-    try {
-        let data = await models.mas_name_titles.findAll();
-    // if (get_nametitle == null) {
-    //   const error = new Error(messages.error);
-    //   throw error;
-    // }
-    result(res, data);
-    } catch (error) {
-        next(error);
-    }
+  try {
+    result(res, await GetAllTitleNameService());
     
-}
-     
+  } catch (error) {
+    next(error);
+  }
+};
