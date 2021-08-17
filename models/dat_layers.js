8c0f4fec6d452977632426dde897e0512b504790
folksriharach <folksriharach@gmail.com>
2021-08-17 13:30:45 +0700

    db

---
+++ models/dat_layers.js
@@ -0,0 +1,76 @@
+const Sequelize = require('sequelize');
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('dat_layers', {
+    id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      defaultValue: DataTypes.UUIDV4,
+      comment: "รหัสหลักจัดการข้อมูลพื้นที่",
+      primaryKey: true
+    },
+    group_layer_id: {
+      type: DataTypes.UUID,
+      allowNull: true,
+      comment: "รหัสกลุ่มข้อมูลพื้นที่",
+      references: {
+        model: 'mas_layer_groups',
+        key: 'id'
+      }
+    },
+    layer_name: {
+      type: DataTypes.STRING(255),
+      allowNull: true,
+      comment: "ชื่อข้อมูลพื้นที่"
+    },
+    wms: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    url: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    created_by: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      comment: "สร้างข้อมูลโดย",
+      references: {
+        model: 'sysm_users',
+        key: 'id'
+      }
+    },
+    created_date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      comment: "สร้างข้อมูลวันที่"
+    },
+    update_by: {
+      type: DataTypes.UUID,
+      allowNull: true,
+      comment: "แก้ไขข้อมูลโดย",
+      references: {
+        model: 'sysm_users',
+        key: 'id'
+      }
+    },
+    update_date: {
+      type: DataTypes.DATE,
+      allowNull: true,
+      comment: "แก้ไขข้อมูลวันที่"
+    }
+  }, {
+    sequelize,
+    tableName: 'dat_layers',
+    schema: 'ptt_data',
+    timestamps: false,
+    indexes: [
+      {
+        name: "dat_layers_pkey",
+        unique: true,
+        fields: [
+          { name: "id" },
+        ]
+      },
+    ]
+  });
+};
--- models/init-models.js
+++ models/init-models.js
@@ -1,5 +1,6 @@
 var DataTypes = require("sequelize").DataTypes;
 var _SequelizeMeta = require("./SequelizeMeta");
+var _dat_layers = require("./dat_layers");
 var _dat_profile_users = require("./dat_profile_users");
 var _mas_layer_groups = require("./mas_layer_groups");
 var _mas_name_titles = require("./mas_name_titles");
@@ -8,14 +9,21 @@ var _sysm_users = require("./sysm_users");
 
 function initModels(sequelize) {
   var SequelizeMeta = _SequelizeMeta(sequelize, DataTypes);
+  var dat_layers = _dat_layers(sequelize, DataTypes);
   var dat_profile_users = _dat_profile_users(sequelize, DataTypes);
   var mas_layer_groups = _mas_layer_groups(sequelize, DataTypes);
   var mas_name_titles = _mas_name_titles(sequelize, DataTypes);
   var sysm_roles = _sysm_roles(sequelize, DataTypes);
   var sysm_users = _sysm_users(sequelize, DataTypes);
 
+  dat_layers.belongsTo(mas_layer_groups, { as: "group_layer", foreignKey: "group_layer_id"});
+  mas_layer_groups.hasMany(dat_layers, { as: "dat_layers", foreignKey: "group_layer_id"});
   dat_profile_users.belongsTo(mas_name_titles, { as: "name_title", foreignKey: "name_title_id"});
   mas_name_titles.hasMany(dat_profile_users, { as: "dat_profile_users", foreignKey: "name_title_id"});
+  dat_layers.belongsTo(sysm_users, { as: "created_by_sysm_user", foreignKey: "created_by"});
+  sysm_users.hasMany(dat_layers, { as: "dat_layers", foreignKey: "created_by"});
+  dat_layers.belongsTo(sysm_users, { as: "update_by_sysm_user", foreignKey: "update_by"});
+  sysm_users.hasMany(dat_layers, { as: "update_by_dat_layers", foreignKey: "update_by"});
   dat_profile_users.belongsTo(sysm_users, { as: "created_by_sysm_user", foreignKey: "created_by"});
   sysm_users.hasMany(dat_profile_users, { as: "dat_profile_users", foreignKey: "created_by"});
   dat_profile_users.belongsTo(sysm_users, { as: "update_by_sysm_user", foreignKey: "update_by"});
@@ -31,6 +39,7 @@ function initModels(sequelize) {
 
   return {
     SequelizeMeta,
+    dat_layers,
     dat_profile_users,
     mas_layer_groups,
     mas_name_titles,
