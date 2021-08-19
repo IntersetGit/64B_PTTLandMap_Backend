e9feaa2aa0fbd0578c9c6ba6d6dfd62d2e09278c
yochi2020 <yochi.suz@gmail.com>
2021-08-19 15:37:27 +0700

    .

---
+++ routes/master.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const masterControllers = require('../controllers/masterControllers');
+
+/** เรียกข้อมูล คำนำหน้าชื่อ */
+router.get('/getNameTitle',masterControllers.getNameTitle);
+router.get('/viewGetNameTitle',masterControllers.viewGetNameTitle);
+/**  **/
+
+
+
+
+
+/** เพิ่่ม ลบ แก้ไข dat_layers (หัวข้อย่อย)  **/
+router.post('/datLayers',masterControllers.createDataLayers);
+router.put('/datLayers',masterControllers.updateDataLayers);
+router.delete('/datLayers',masterControllers.deleteDataLayers);
+/** ***/
+
+module.exports = router;
