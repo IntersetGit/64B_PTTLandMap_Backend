85f46ed29d6838b7bbcf299c87e20c3a28f3e096
yochi2020 <yochi.suz@gmail.com>
2021-08-23 18:41:05 +0700

    .

--- routes/demo.js
+++ routes/demo.js
@@ -1,8 +1,17 @@
 const router = require('express').Router();
 const { demoLdap } = require('../controllers/demoControllers');
 const { authenticateToken } = require('../middleware/authenticateToken');
-
+const  shapefile =require("shapefile")
 
 router.post('/demoLdap', [authenticateToken], demoLdap);
-
+router.get('/demoGoJson',(req,res,)=>{
+    shapefile.open("public/testShapfile/shape/buildings.shp")
+    .then(source => source.read()
+      .then(function log(result) {
+        if (result.done) return;
+        console.log(result.value);
+        res.json({shp:result.value})
+      }))
+    .catch(error => console.error(error.stack));
+})
 module.exports = router;
