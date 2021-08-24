3b0ab27f89af6ffbd7843d59cc4d2a1aa8c5f60e
yochi2020 <yochi.suz@gmail.com>
2021-08-24 16:06:33 +0700

    .

--- routes/demo.js
+++ routes/demo.js
@@ -1,17 +1,31 @@
-const router = require('express').Router();
-const { demoLdap } = require('../controllers/demoControllers');
-const { authenticateToken } = require('../middleware/authenticateToken');
-const  shapefile =require("shapefile")
+const router = require("express").Router();
+const { demoLdap } = require("../controllers/demoControllers");
+const { authenticateToken } = require("../middleware/authenticateToken");
+const shapefile = require("shapefile");
+const { convert } = require("geojson2shp");
+router.post("/demoLdap", [authenticateToken], demoLdap);
 
-router.post('/demoLdap', [authenticateToken], demoLdap);
-router.get('/demoGoJson',(req,res,)=>{
-    shapefile.open("public/testShapfile/shape/buildings.shp")
-    .then(source => source.read()
-      .then(function log(result) {
+router.get("/demoGoJson", (req, res) => {
+  shapefile
+    .open("public/testShapfile/shape/buildings.shp")
+    .then((source) =>
+      source.read().then(function log(result) {
         if (result.done) return;
         console.log(result.value);
-        res.json({shp:result.value})
-      }))
-    .catch(error => console.error(error.stack));
-})
+        res.json({ shp: result.value });
+      })
+    )
+    .catch((error) => console.error(error.stack));
+});
+
+router.get("/demoShape", async (req, res) => {
+  const options = {
+    layer: "my-layer",
+    targetCrs: 2154,
+  };
+  const features = [
+    req.body
+  ];
+  res.json(await convert(features, "public/testShapfile/shape/gojson2shape.zip", options))
+});
 module.exports = router;
