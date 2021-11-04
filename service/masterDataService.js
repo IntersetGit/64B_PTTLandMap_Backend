88465ce1944c6ca300ab10eb1927fd84a672e9e6
sriharach <folksriharach@gmail.com>
2021-11-05 03:31:59 +0700

    .

--- service/masterDataService.js
+++ service/masterDataService.js
@@ -209,8 +209,12 @@ exports.editMasLayersShapeService = async (data, user) => {
 exports.deleteMasLayersShapeService = async (data, user) => {
   const getGisPk = await models.mas_layers_shape.findByPk(data.id);
   const getAllDB = await sequelizeString(`select * from information_schema.tables`);
-  const { table_schema, table_name} =  getAllDB.find(x => x.table_name == getGisPk.table_name);
-  await sequelizeStringFindOne(` DROP TABLE ${table_schema}.${table_name} `);
+
+  if(getGisPk.table_name) {
+    const { table_schema, table_name} =  getAllDB.find(x => x.table_name == getGisPk.table_name);
+    await sequelizeStringFindOne(` DROP TABLE ${table_schema}.${table_name} `);
+  }
+  
   await models.mas_layers_shape.destroy({
     where: { id: data.id }
   })
