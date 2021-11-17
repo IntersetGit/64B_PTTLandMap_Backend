faa9b68f9fd7716b31a051925699dfeb25552389
sriharach <folksriharach@gmail.com>
2021-11-17 13:03:44 +0700

    swagger

---
+++ swagger/streetview.js
@@ -0,0 +1,21 @@
+const Joi = require("@hapi/joi");
+/**
+ * File name for request and response model should be same as router file.
+ * Define request model with there order in router js file.
+ * For example first api in user router is create user so we define createUser schema with key 0.
+ */
+module.exports = {
+    1: {
+        body: {
+            coordinate: {
+                lat:Joi.string(),log:Joi.string()
+            },
+            name:Joi.string(),
+            url: Joi.string()
+        },
+
+        model: "streetview 1", // Name of the model
+        group: "streetview",
+        description: "",
+    },
+};
